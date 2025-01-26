c55c9a408530c695009d0f72f5f61d7d9d5b5e92
Dpbm <dpbm136@gmail.com>
2025-01-26 20:40:19 -0300

    adding CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    `java-library`
     `maven-publish`
+    signing
 }
 
 group = "org.quantum"
@@ -10,6 +12,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
 dependencies {
     testImplementation(kotlin("test"))
 }
@@ -17,6 +24,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
@@ -31,10 +39,74 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
+
+        /*maven{
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
+        }*/
     }
     publications {
         register<MavenPublication>("gpr") {
             from(components["java"])
         }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "qko"
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name = "QKO"
+                description = "A open source library for simulating your quantum circuits with Kotlin."
+                url = "https://github.com/Dpbm/qko"
+              
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id = "Dpbm"
+                        name = "Alexandre Silva"
+                        email = "dpbm136@gmail.com"
+                    }
+                }
+                
+                scm {
+                    connection = "scm:git:git://github.com/Dpbm/qko.git"
+                    developerConnection = "scm:git:ssh://github.com/Dpbm/qko.git"
+                    url = "http://github.com/Dpbm/qko/"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
---
+++ readme.md
@@ -0,0 +1,37 @@
+# QKO
+## A quantum library for Kotlin
+
+![build and test workflow](https://github.com/Dpbm/qko/actions/workflows/build-test.yml/badge.svg)
+![publish workflow](https://github.com/Dpbm/qko/actions/workflows/publish.yml/badge.svg)
+
+Qko is an open source library for creating quantum circuits and executing them easily.
+
+Its usage is inspired by `Qiskit` and `Cirq`. So creating your own circuit must be easy as using one of the mainstream frameworks.
+
+To Create a Bell State for example just run:
+
+```kotlin
+package examples
+
+import Circuit
+import providers.Outcome
+import providers.local.LocalBackend
+import providers.local.gates.H
+import providers.local.gates.CNOT
+
+fun main(){
+    val circuit = Circuit(2) //circuit with 2 qubits
+    circuit.addGate(H(arrayListOf(0)))
+    circuit.addGate(CNOT(arrayListOf(0,1)))
+
+    val backend = LocalBackend()
+    val result:Outcome = backend.execute(circuit)
+
+    println("---OUTPUT---")
+
+    for((index,prob) in result.toDist().withIndex()){
+        val binaryRep = index.toString(radix = 2).padStart(2, '0')
+        println("$binaryRep $prob")
+    }
+}
+```
